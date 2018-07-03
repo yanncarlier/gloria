039b6d2ebdf443914716907c3fa29ef8d610bc19
Yann Carlier <carlier@gmail.com>
2018-07-03 21:38:19 +0800

    add README.md sha256sum

--- gloria.py
+++ gloria.py
@@ -6,5 +6,5 @@ __credits__ = ["Yann Carlier"]
 __license__ = "MIT"
 __version__ = "0.0.1"
 __maintainer__ = "Yann Carlier"
-__email__ = "yanncarlier at gmail.com"
+__email__ = "yanncarlier at gmail dot com"
 __status__ = "Pre-alpha"
